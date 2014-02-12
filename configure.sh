14b667f5f86425edbdaec5a324a92b78368bf0fc
rhaas <rhaas@5c1b1641-b641-47ec-b3d9-0f93d4c445ee>
2014-02-12 22:21:47 +0000

    report missing PATCH or TAR
    git-svn-id: http://svn.cactuscode.org/projects/ExternalLibraries/LORENE/trunk@50 5c1b1641-b641-47ec-b3d9-0f93d4c445ee

--- configure.sh
+++ configure.sh
@@ -39,6 +39,23 @@ then
     echo "Using bundled LORENE..."
     echo "END MESSAGE"
     
+    # check for required tools. Do this here so that we don't require them when
+    # using the system library
+    if [ x$TAR = x ] ; then
+      echo 'BEGIN ERROR'
+      echo 'Could not find tar command. Please make sure that (gnu) tar is present'
+      echo 'and that the TAR variable is set to its location.'
+      echo 'END ERROR'
+      exit 1
+    fi
+    if [ x$PATCH = x ] ; then
+      echo 'BEGIN ERROR'
+      echo 'Could not find patch command. Please make sure that (gnu) tar is present'
+      echo 'and that the PATCH variable is set to its location.'
+      echo 'END ERROR'
+      exit 1
+    fi
+
     # Set locations
     THORN=LORENE
     NAME=Lorene
